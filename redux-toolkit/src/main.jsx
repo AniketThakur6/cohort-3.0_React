cb0d96cdc280aea51b7ebf7fcb44b329a8af15b5
AniketThakur6 <aniketthakur1928@gmail.com>
2026-09-07 21:16:05 +0530

    fix: format main.jsx for better readability

--- redux-toolkit/src/main.jsx
+++ redux-toolkit/src/main.jsx
@@ -5,7 +5,9 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
+  
   <Provider store={store}>
     <App />
   </Provider>,
+
 );
